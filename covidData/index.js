dacc2d4f306f0c5a309ebed2e8835fd2901f365f
Joey Cadieux <58401407+drjoeycadieux@users.noreply.github.com>
2022-01-10 23:04:12 +0000

    inm.

--- covidData/index.js
+++ covidData/index.js
@@ -3,21 +3,18 @@
  * @author joey@tech-center.com (Joey Cadieux)
  */
 
-// dailyOverview[0] gets the first object, wrapped in braces {}.
-// dailyOverview[1] would get the second object in braces.
-// adding '.cases', '.newCases', etc. gets the property you want.
 
-const dailyOverview = [
-  {
-    date: "2021-09-15",
-    cases: 399058, 
-    deaths: 11312, 
-    hospitalized: 1125
-  }
-];
+var totalCases = 0;
+var totalVaccinations = 0;
+var hospitalizations = 0;
 
-// dailyOverview ->
 
-document.getElementById("cases").innerHTML = dailyOverview[0].cases;
-document.getElementById("deaths").innerHTML = dailyOverview[0].deaths;
-document.getElementById("hospitalized").innerHTML = dailyOverview[0].hospitalized;
+$(document).ready(() => {
+  $.ajax({
+    url: api_url + "https://api.covid19tracker.ca/summary"
+  }).then(res => {
+    var data = res.data[0];
+
+    document.getElementById("totalCases").innerHTML = dailyOverview[0].totalCases;
+    document.getElementById("totalVaccinations").innerHTML = dailyOverview[0].totalVaccinations;
+    document.getElementById("hospitalizations").innerHTML = dailyOverview[0].hospitalizations;
