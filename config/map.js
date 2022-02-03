33d87c4f8022b9e1496b3a095f6683f59e19f5f1
Joey Cadieux <58401407+drjoeycadieux@users.noreply.github.com>
2022-02-03 19:47:43 +0000

    inm.

---
+++ config/map.js
@@ -0,0 +1,7 @@
+mapboxgl.accessToken = 'pk.eyJ1Ijoiam9leWNyZWF0b3IiLCJhIjoiY2t6N2RvaWZkMGkycDJ2bDRhNWFtYTBicSJ9.QXg8HSadej3YG5qrQcy-NA';
+const map = new mapboxgl.Map({
+  container: 'map',
+  style: 'mapbox://styles/mapbox/empty-v8',
+  center: [-106.3468, 56.1304],
+  zoom: 9
+});
